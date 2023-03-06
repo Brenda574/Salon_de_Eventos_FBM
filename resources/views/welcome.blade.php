f1e972c5fcb78a18b68f506da7667ff2bc9f13ac
Brenda Perez <l19270574@tuxtla.tecnm.mx>
2023-03-05 23:05:35 -0600

    tercer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">BRENDA</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    AQUI MODIFIQUE YOOOOOOOO
+                                    AQUI MODIFIQUE otra vez yo
                                 </p>
                             </div>
 
