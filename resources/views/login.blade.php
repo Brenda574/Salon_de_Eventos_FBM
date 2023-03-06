0417e500431c29746d71dd2925c52fdb89ae48f6
Fabricio Cruz <cruzfabrizio7@gmail.com>
2023-03-06 17:42:44 -0600

    boton commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,7 +13,7 @@
           
           <div class="row d-flex justify-content-center align-items-center h-100" >
             <div class="col-xl-10">
-              <div class="card rounded-3 text-black">
+              <div class="card rounded-3 text-black" style="background-color: #b5c1b7;">
                 <div class="row g-0">
                   <div class="col-lg-6">
                     <div class="card-body p-md-5 mx-md-4">
@@ -39,14 +39,14 @@
                         </div>
       
                         <div class="text-center pt-1 mb-5 pb-1">
-                          <div><button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Iniciar
+                          <div><button class="btn btn btn-outline-dark-custom-2 mb-3" type="button" style="background-color: #E1D5D5; border:none; transition: all 0.35s;">Iniciar
                             sesión</button></div>
                           <a class="text-muted" href="#!">¿Olvidaste tu contraseña?</a>
                         </div>
       
                         <div class="d-flex align-items-center justify-content-center pb-4">
                           <p class="mb-0 me-2">¿No tienes cuenta?</p>
-                          <button type="button" class="btn btn-outline-danger">Crear nueva</button>
+                          <button type="button" class="btn btn-outline-dark" style="background-color: #E1D5D5; transition: all 0.35s;">Crear nueva</button>
                         </div>
       
                       </form>
