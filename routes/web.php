42e5e22df1639c5bf8ebf2bcfa236f9576603325
Brenda Perez <l19270574@tuxtla.tecnm.mx>
2023-03-06 18:22:18 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SistemaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('principal');
 });
+
+Route::get('/Login',[SistemaController::class, 'login'])->name('login');
