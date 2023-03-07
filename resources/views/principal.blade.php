bcec073178a1f037b78799e4fef6a9cc5c882ae9
Brenda Perez <l19270574@tuxtla.tecnm.mx>
2023-03-07 02:01:37 -0600

    cambios

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -14,8 +14,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Bodas</h5>
                     <p class="card-text">
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
+                        This is a wider carntent is a little bit longer.
                     </p>
                 </div>
                 <div class="card-footer">
