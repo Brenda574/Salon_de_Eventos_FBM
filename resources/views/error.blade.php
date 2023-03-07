ee6a41aef322945659d1f1db074ceb1f66950eea
Brenda Perez <l19270574@tuxtla.tecnm.mx>
2023-03-07 00:48:15 -0600

    cambios

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -14,36 +14,13 @@
     <section>
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-xl-10">
-                    <div class="card rounded-3 text-black" style="background-color: #b5c1b7;">
-                        <div class="row g-0">
-                            <div class="col-lg-6">
-                                <div class="card-body p-md-5 mx-md-4">
-                                    <div class="text-center">
-                                        <img src="https://cdn-icons-png.flaticon.com/512/8915/8915119.png" style="width: 80px;" alt="logo">
-                                    </div>
-                                    <div class="text-center">
-                                        <img src="https://www.aimmedia.com/wp-content/uploads/2022/10/GardenGate_LOGO_FINAL-600x250_554e45.png" style="width: 185px; " alt="logo">
-                                    </div>
-                                    <form style="padding-top: 2rem">
-                                        <div class="form-outline mb-4">
-                                            <input type="email" id="form2Example11" class="form-control" placeholder="Correo electronico" />
-                                            <label class="form-label" for="form2Example11">Usuario</label>
-                                        </div>
-                                        <div class="form-outline mb-4">
-                                            <input type="password" id="form2Example22" class="form-control" />
-                                            <label class="form-label" for="form2Example22">Contraseña</label>
-                                        </div>
-                                        <div class="text-center pt-1 mb-5 pb-1">
-                                            <div><button class="btn boton4 custom-2 mb-3" type="button" style="background-color: #E1D5D5; transition: all 0.35s;">Iniciar sesión</button></div>
-                                            <a class="text-muted" href="#!">¿Olvidaste tu contraseña?</a>
-                                        </div>
-                                        <div class="d-flex align-items-center justify-content-center pb-4">
-                                            <p class="mb-0 me-2">¿No tienes cuenta?</p>
-                                            <button type="button" class="btn boton4" style="background-color: #E1D5D5; transition: all 0.35s;">Crear nueva</button>
-                                        </div>
-                                    </form>
-                                </div>
+                <div class="col-xl-5">
+                    <div class="card text-black" style="background-color: #b5c1b7;">
+                        <div class="row">
+                            <div class="card-body p-md-5 mx-md-4 text-center">
+                                <h2 class="fw-bold mb-2 text-center" style="color:red;">ERROR</h2>
+                                <p class="text-white-50 mb-5">Error al iniciar sesión. Verifique las credenciales de acceso.</p>
+                                <a class="btn btn-outline-light btn-lg px-5" href="{{route('login')}}">Volver al Login</a>
                             </div>
                         </div>
                     </div>
