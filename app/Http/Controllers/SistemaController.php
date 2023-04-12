d2a7688d9a6369cf34bd42cf91f2c77f5c8cac5e
Fabricio Cruz <cruzfabrizio7@gmail.com>
2023-04-12 01:15:03 -0600

    crudUsuario2

--- app/Http/Controllers/SistemaController.php
+++ app/Http/Controllers/SistemaController.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
-
 class SistemaController extends Controller
 {
     public function login()
