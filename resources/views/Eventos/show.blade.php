ffb7bc517cf0ae98c5b220fe20202f1f79e5af60
Brenda Perez <l19270574@tuxtla.tecnm.mx>
2023-05-26 17:55:48 -0600

    revision2

--- resources/views/Eventos/show.blade.php
+++ resources/views/Eventos/show.blade.php
@@ -221,7 +221,7 @@
             </div>
         </div>
     </div>
-    <!-- ------ -->
+    
     <div class="modal fade" id="agregarFoto" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
         aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
