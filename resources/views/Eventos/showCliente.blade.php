31c31c6c0467651aab617ade9eaba669790bafad
Brenda Perez <l19270574@tuxtla.tecnm.mx>
2023-05-27 01:42:52 -0600

    commits

--- resources/views/Eventos/showCliente.blade.php
+++ resources/views/Eventos/showCliente.blade.php
@@ -221,7 +221,10 @@
                         enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
-                            <input class="form-control" type="file" name="archivo" accept="image/*" id="archivo">
+                            <input class="form-control" type="file" name="archivo" id="archivo" accept="image/*">
+                        </div>
+                        <div>
+                            <textarea id="descrip" name="descrip" placeholder="Ingrese una descripción" class="form-control"></textarea>
                         </div>
                         <div>
                             <textarea id="descrip" name="descrip" placeholder="Ingrese una descripción" class="form-control"></textarea>
